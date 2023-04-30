76ff50b17010365a95f089c2749d5b02821fadf6
Rupesh malhotra <60328334+Rupesh0914@users.noreply.github.com>
2023-04-30 00:05:46 -0400

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import Main from './components/Home';
+import { DataProvider } from './context/dataContext'
+
+
+export default function Home() {
+
+  return (
+    <div>
+      <DataProvider>
+        <Main/>
+      </DataProvider>
+    </div>
+  );
+}
